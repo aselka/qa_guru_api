e67b1009072b2053b9c96a68faffbb2a4c0472d7
aselka <bis-aselya@yandex.ru>
2023-04-08 18:50:10 +0400

    new commit with changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 import os
-
 import pytest
 from dotenv import load_dotenv
 from selene.support.shared import browser
-
-from framework.demoqa_with_env import DemoQaWithEnv
+from framework import demoqa_with_env
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from utils import attachments
 
 load_dotenv()
 
@@ -20,7 +21,7 @@ def env(request):
 
 @pytest.fixture(scope='session')
 def demoshop(env):
-    return DemoQaWithEnv(env)
+    return demoqa_with_env(env)
 
 
 @pytest.fixture(scope='session')
@@ -32,15 +33,35 @@ def cookie(demoshop):
 
 @pytest.fixture(scope='function')
 def app(demoshop, cookie):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser.config.driver = driver
     browser.config.base_url = demoshop.demoqa.url
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.open("Themes/DefaultClean/Content/images/logo.png")
     browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
     yield browser
+    attachments.add_html(browser)
+    attachments.add_screenshot(browser)
+    attachments.add_logs(browser)
+    attachments.add_video(browser)
+    browser.quit()
     browser.quit()
 
 
 @pytest.fixture(scope='session')
 def reqres(env):
-    return DemoQaWithEnv(env).reqres
+    return demoqa_with_env(env).reqres
--- tests/test_demoshop.py
+++ tests/test_demoshop.py
@@ -2,12 +2,18 @@ import allure
 from selene import have
 
 
+@allure.story('Demowebshop tests')
+@allure.feature('Login')
+@allure.title("Successful auth")
 def test_auth(app):
     app.open("")
     with allure.step('Check successful auth'):
         app.element(".account").should(have.text("bis-aselya@yandex.ru"))
 
 
+@allure.story('Demowebshop tests')
+@allure.feature('Wishlist')
+@allure.title("Delete product from wishlist")
 def test_delete_product_from_wishlist(demoshop, app):
     app.open("")
     with allure.step('Add product to wishlist'):
@@ -16,17 +22,25 @@ def test_delete_product_from_wishlist(demoshop, app):
         app.element('.ico-wishlist').click()
         app.element('[name="removefromcart"]').click()
         app.element('[name="updatecart"]').click()
+    with allure.step('Check delete product from wishlist'):
         app.element('.wishlist-content').should(have.text('The wishlist is empty!'))
 
 
+@allure.story('Demowebshop tests')
+@allure.feature('Wallet')
+@allure.title("Add product to cart")
 def test_add_product_to_cart(demoshop, app):
     app.open("")
     with allure.step("Check successful add to cart"):
         demoshop.add_product_to_cart()
         app.element(".ico-cart").click()
+    with allure.step("Checking the cart has been added"):
         app.element(".product-name").should(have.text("Laptop"))
 
 
+@allure.story('Demowebshop tests')
+@allure.feature('Wallet')
+@allure.title("Remove product to cart")
 def test_delete_product_from_cart(demoshop, app):
     app.open("")
     with allure.step('Add product to cart'):
@@ -35,11 +49,16 @@ def test_delete_product_from_cart(demoshop, app):
         app.element('.ico-cart').click()
         app.element('[name="removefromcart"]').click()
         app.element('[name="updatecart"]').click()
+    with allure.step("Checking the cart has been deleted"):
         app.element('.order-summary-content').should(have.text('Your Shopping Cart is empty!'))
 
 
+@allure.story('Demowebshop tests')
+@allure.feature('Logout')
+@allure.title("Successful logout")
 def test_logout(app):
     app.open("")
-    with allure.step('Check successful logout'):
+    with allure.step('Logout'):
         app.element('.ico-logout').click()
+    with allure.step('Check successful logout'):
         app.element('.ico-login').should(have.text('Log in'))
